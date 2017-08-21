474794e853e5911a6a79739404fd6cd46d018cfe
Tyler Lee <tlee753@gmail.com>
2017-08-21 13:44:00 -0400

    updates

--- fen-viewer/FENViewer.java
+++ fen-viewer/FENViewer.java
@@ -1,5 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class FENViewer {
     private String position;
+    private ArrayList<ArrayList<char>> positionArray;
 
     public FENViewer(String position) {
         // trimming, parsing, and correction
@@ -10,7 +14,20 @@ public class FENViewer {
         if (position == null) {
             System.out.println("No position provided.");
         } else {
-
+            int boardPosition = 0;
+            for (int i = 0; i < position.length; i++) {
+                if (position[i].isDigit()) {
+                    for (int j = 0; j < position[i]; j++) {
+                        // insert null space on board
+                    }
+                } else if (position[i] == "/") {
+                    // error checking
+                } else {
+                    positionArray[i % 8][i / 8] = position[i];
+                }
+            }
         }
+        System.out.println(positionArray);
     }
+
 }
