6f3ba5f07580beef4e08e2a8752034a305e9f9e2
Tyler Lee <Tyler@Tylers-MacBook-Pro-3.local>
2017-10-14 19:37:25 -0400

    almost there

---
+++ src/ChessApp.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+
+// Scene
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+// Stage
+import javafx.stage.Stage;
+
+
+public class ChessApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ChessViewer-1.0.fxml"));
+        Scene scene = new Scene(root, 1200, 675);
+
+        stage.setTitle("Chess Viewer 1.0");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/ChessController.java
@@ -0,0 +1,43 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ChessController {
+
+    @FXML
+    private Pane a1, a2, a3, a4, a5, a6, a7, a8, b1, b2, b3, b4, b5, b6, b7, b8,
+                 c1, c2, c3, c4, c5, c6, c7, c8, d1, d2, d3, d4, d5, d6, d7, d8,
+                 e1, e2, e3, e4, e5, e6, e7, e8, f1, f2, f3, f4, f5, f6, f7, f8,
+                 g1, g2, g3, g4, g5, g6, g7, g8, h1, h2, h3, h4, h5, h6, h7, h8;
+
+    @FXML
+    private Button positionButton;
+
+    @FXML
+    private TextField positionField;
+
+    @FXML
+    void initialize() {
+        Pane[] lightSquares = {a2, a4, a6, a8, b1, b3, b5, b7, c2, c4, c6, c8, d1, d3, d5, d7, e2, e4, e6, e8, f1, f3, f5, f7, g2, g4, g6, g8, h1, h3, h5, h7};
+        Pane[] darkSquares = {a1, a3, a5, a7, b2, b4, b6, b8, c1, c3, c5, c7, d2, d4, d6, d8, e1, e3, e5, e7, f2, f4, f6, f8, g1, g3, g5, g7, h2, h4, h6, h8};
+
+        for (Pane p : lightSquares) {
+            p.setStyle("-fx-background-color: #fff;");
+        }
+        for (Pane p : darkSquares) {
+            p.setStyle("-fx-background-color: #bbb;");
+        }
+        System.out.println("initialized...");
+    }
+
+    @FXML
+    void heatMap() {
+        FENViewer viewer = new FENViewer(positionField.getText());
+        int[][] pointArray = viewer.getPointArray();
+    }
+}
