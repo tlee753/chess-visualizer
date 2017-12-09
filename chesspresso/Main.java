7d5c98e5d728de78a6b5121b409c22b394cf4859
Tyler Lee <tlee753@gmail.com>
2017-12-09 02:07:05 -0500

    updates

--- chesspresso/Main.java
+++ chesspresso/Main.java
@@ -1,4 +1,3 @@
-import chesspresso.game.view.GameTextViewer;
 import chesspresso.pgn.PGNReader;
 import chesspresso.pgn.PGNSyntaxError;
 import chesspresso.game.Game;
@@ -9,8 +8,8 @@ public class Main {
 	public static void main(String[] args) throws PGNSyntaxError, IOException {
 		System.out.println("Program starting...");
 
-        InputStream inputStream = new FileInputStream("GrandpaVsTyler-2.pgn");
-        PGNReader pgnReader = new PGNReader(inputStream, "GrandpaVsTyler-2.pgn");
+        InputStream inputStream = new FileInputStream("GrandpaVsTyler-4.pgn");
+        PGNReader pgnReader = new PGNReader(inputStream, "GrandpaVsTyler-4.pgn");
 
         Game game = pgnReader.parseGame();
 
--- notes.md
+++ notes.md
@@ -4,3 +4,9 @@ jar -cvfm Chess-<version>-<java version>.jar manifest.txt *.class *.fxml assets
 ### Java Compilation
 javac -target 1.8 -source 1.8 *.java
 
+
+### Chesspresso Testing
+javac -cp .:Chesspresso-lib.jar Main.java
+
+### Chesspresso Testing
+java -cp .:Chesspresso-lib.jar Main
